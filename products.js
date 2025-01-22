5a2e4e0dd036b97af00c9f2ddceb6e0954416f0e
DavidAkerlind <david.akerlind@icloud.com>
2025-01-22 17:19:51 +0100

    Implement shopping bag functionality with quantity tracking and dropdown display

--- products.js
+++ products.js
@@ -8,6 +8,7 @@ const menu = [
         price: 29,
         rating: 4.5,
         image: "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D",
+        quantity: 1,
     },
     {
         id: 2,
@@ -18,6 +19,7 @@ const menu = [
         price: 59,
         rating: 3.9,
         image: "https://images.unsplash.com/photo-1621884946220-6dd5e2ad51a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGRvcHBpb3xlbnwwfHwwfHx8MA%3D%3D",
+        quantity: 1,
     },
     {
         id: 3,
@@ -28,6 +30,7 @@ const menu = [
         price: 49,
         rating: 4.3,
         image: "https://plus.unsplash.com/premium_photo-1673545518947-ddf3240090b1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FwcHVjaGlub3xlbnwwfHwwfHx8MA%3D%3D",
+        quantity: 1,
     },
     {
         id: 4,
@@ -38,6 +41,7 @@ const menu = [
         price: 49,
         rating: 4.6,
         image: "https://images.unsplash.com/photo-1485808191679-5f86510681a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFjY2hpYXRvfGVufDB8fDB8fHww",
+        quantity: 1,
     },
     {
         id: 5,
@@ -48,6 +52,7 @@ const menu = [
         price: 59,
         rating: 4.4,
         image: "https://images.unsplash.com/photo-1604313803931-169516ec5498?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        quantity: 1,
     },
     {
         id: 6,
@@ -58,6 +63,7 @@ const menu = [
         price: 55,
         rating: 4.8,
         image: "https://images.unsplash.com/photo-1534687941688-651ccaafbff8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29ydGFkb3xlbnwwfHwwfHx8MA%3D%3D",
+        quantity: 1,
     },
     {
         id: 7,
@@ -68,6 +74,7 @@ const menu = [
         price: 69,
         rating: 4.9,
         image: "https://images.unsplash.com/photo-1517701604599-bb29b565090c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWFjY2hpYXRvfGVufDB8fDB8fHww",
+        quantity: 1,
     },
 ];
 
@@ -123,7 +130,52 @@ addCoffeeSort(menu);
 
 let shoppingBag = [];
 
-let allButtons = document.querySelectorAll("button");
+function addToShoppingBag(button) {
+    const productId = parseInt(button.id, 10);
+    const existingProduct = shoppingBag.find((item) => item.id === productId);
+
+    if (existingProduct) {
+        existingProduct.quantity++;
+    } else {
+        shoppingBag.push(menu[button.id - 1]);
+    }
+
+    console.log(shoppingBag);
+    let divShoppingBagAmountRef = document.querySelector(
+        "#divShoppingBagAmount"
+    );
+    divShoppingBagAmountRef.classList.remove("d-none");
+    updateBagItemAmount();
+}
+
+function showShoppingPage() {
+    document.querySelector(".menu-header1").classList.add("d-none");
+    document.querySelector(".menu-container").classList.add("d-none");
+    document.querySelector(".menu-header1").classList.add("d-none");
+    document.querySelector(".menu-header2").classList.remove("d-none");
+}
+
+//==============================Not functions ================================
+let shoppingBagBtn = document.querySelector(".shopping-bag");
+
+shoppingBagBtn.addEventListener("mouseenter", () => {
+    shoppingBagBtn.classList.add("shopping-bag-hover");
+    updateBagDropdown();
+    document.querySelector(".dropdown").classList.remove("d-none");
+});
+
+shoppingBagBtn.addEventListener("mouseleave", () => {
+    shoppingBagBtn.classList.remove("shopping-bag-hover");
+});
+
+shoppingBagBtn.addEventListener("click", showShoppingPage);
+document.querySelector("#checkout").addEventListener("click", showShoppingPage);
+
+document.querySelector("#bagDropdown").addEventListener("mouseleave", () => {
+    document.querySelector("#bagDropdown").classList.add("d-none");
+});
+
+let allButtons = document.querySelectorAll(".add-btn");
 allButtons.forEach((button) => {
     button.addEventListener("mouseenter", () =>
         button.classList.toggle("add-btn-hover")
@@ -134,7 +186,77 @@ allButtons.forEach((button) => {
     );
 
     button.addEventListener("click", () => {
-        shoppingBag.push(menu[button.id]);
-        console.log(shoppingBag);
+        addToShoppingBag(button);
     });
 });
+
+let bagDropdownRef = document.querySelector("#bagDropdown");
+let bagItemsList = document.querySelector("#bagItems");
+
+//==============================^^^^^ Not functions ^^^^ ================================
+
+function updateBagDropdown() {
+    bagItemsList.innerHTML = "";
+    if (shoppingBag.length === 0) {
+        let emptyMsg = document.createElement("li");
+        emptyMsg.textContent = "Varukorgen är tom";
+        bagItemsList.appendChild(emptyMsg);
+        return;
+    }
+
+    shoppingBag.forEach((item) => {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        const fig = document.createElement("figure");
+        const img = document.createElement("img");
+        const span = document.createElement("span");
+        const button = document.createElement("button");
+
+        img.src = item.image;
+        p.classList.add("p-dropdown");
+        fig.classList.add("fig-dropdown");
+        img.classList.add("img-dropdown");
+        span.classList.add("span-dropdown");
+        button.classList.add("remove-btn-dropdown");
+        button.dataset.id = `${item.id}`;
+
+        p.textContent = `${item.quantity} st`;
+        button.textContent = "Ta bort";
+        span.textContent = `${item.title}${item.price}kr`;
+
+        button.addEventListener("click", () => {
+            const itemId = parseInt(button.dataset.id);
+            console.log(itemId);
+            removeItemFromCart(itemId);
+        });
+
+        li.appendChild(p);
+        li.appendChild(fig);
+        fig.appendChild(img);
+        li.appendChild(span);
+        li.appendChild(button);
+
+        bagItemsList.appendChild(li);
+        if (item.quantity > 1) {
+            return;
+        }
+    });
+}
+
+function removeItemFromCart(itemId) {
+    console.log("removeItemFromCart()");
+    shoppingBag = shoppingBag.filter((item) => item.id !== itemId);
+    updateBagItemAmount();
+    updateBagDropdown();
+    console.log(shoppingBag);
+}
+
+function updateBagItemAmount() {
+    let bagAmount = 0;
+    shoppingBag.forEach((item) => {
+        console.log(item.quantity);
+        bagAmount = bagAmount + parseInt(item.quantity);
+    });
+
+    document.querySelector("#divShoppingBagAmount").textContent = bagAmount;
+}
